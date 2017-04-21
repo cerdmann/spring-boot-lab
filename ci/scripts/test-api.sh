d293c635c2c70403452e712b7861594bbed96268
cerdmann <erds@badmoondev.com>
2017-04-20 21:38:07 -0500

    Starting testing

---
+++ ci/scripts/test-api.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+export TERM=${TERM:-dumb}
+
+echo "=============================================="
+echo "Beginning Test of API"
+echo "=============================================="
+
+cd git-repo
+
+echo "Test goes here"
+
+echo "----------------------------------------------"
+echo "Test Complete"
+echo "----------------------------------------------"
